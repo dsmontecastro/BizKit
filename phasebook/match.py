417d965cd58c4e05f4b5867a339b9e2ce5a5c2f4
D2 <dsmontecastro1@up.edu.ph>
2023-12-28 18:21:59 +0800

    Updated Is_Match function

--- phasebook/match.py
+++ phasebook/match.py
@@ -19,9 +19,62 @@ def match(match_id):
     return {"message": msg, "elapsedTime": end - start}, 200
 
 
-def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
+
+
+def is_match(fave_numbers_1: list[int], fave_numbers_2: list[int]):
+    """
+    Match two lists of integers (equality).
+
+    Comparison
+    ----------
+        MATCHES[2]: From ~40+s to ~1s.
+        MATCHES[3]: From ~45+s to <1s.
+
+    Parameters
+    ----------
+        fave_numbers_1 : list[int]
+            numbers to match against
+        fave_numbers_2 : list[int]
+            numbers to compare
+
+    Returns
+    -------
+    boolean
+        whether or not all integers in fave_numbers_2 are in fave_numbers_1
+    """
+    
+    # [ADD] 'Clean' lists for easier processing.
+    faves_1 = clean(fave_numbers_1)
+    faves_2 = clean(fave_numbers_2)
+
+
+    for num in faves_2:
+
+        # [ADD] Remove matched elements from <faves_2>,
+        # ----- to remove redundant comparisons.
+
+        try:
+            ind = faves_1.index(num)
+            faves_1.pop(ind)
+
+        except ValueError: return False
 
     return True
+
+
+
+def clean(nums: list[int]):
+    """
+    'Cleans up' a list of integers.
+
+    Parameters
+    ----------
+    nums: list[int]
+        list to be 'cleaned'
+
+    Returns
+    -------
+        a sorted list of integers without duplicate elements
+    """
+
+    return sorted(set(nums))
